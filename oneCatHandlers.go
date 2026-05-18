53068bb06decfb4f1d4bb11841c3e35c5a96be33
bo-dev2a <bo.dev2a@gmail.com>
2026-05-18 16:02:40 +0200

    Implement getCat and deleteCat endpoints

--- oneCatHandlers.go
+++ oneCatHandlers.go
@@ -3,6 +3,29 @@ package main
 import "net/http"
 
 func getCat(req *http.Request) (int, any) {
-	Logger.Info("Cat not found")
-	return http.StatusNotFound, "Cat not found"
+	catId := req.PathValue("catId")
+
+	cat, ok := catsDatabase[catId]
+	if !ok {
+		Logger.Info("Cat not found: ", catId)
+		return http.StatusNotFound, "Cat not found"
+	}
+
+	Logger.Info("Cat found: ", catId)
+	return http.StatusOK, cat
+}
+
+func deleteCat(req *http.Request) (int, any) {
+	catId := req.PathValue("catId")
+
+	_, ok := catsDatabase[catId]
+	if !ok {
+		Logger.Info("Cat not found: ", catId)
+		return http.StatusNotFound, "Cat not found"
+	}
+
+	delete(catsDatabase, catId)
+
+	Logger.Info("Cat deleted: ", catId)
+	return http.StatusNoContent, nil
 }
